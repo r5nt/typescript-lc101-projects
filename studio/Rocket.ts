8df715b74b6992ceb176424ae2aeb6319e5f1adc
nick <nicholasbhall91@gmail.com>
2020-05-16 16:30:00 -0400

    Finished assignment

---
+++ studio/Rocket.ts
@@ -0,0 +1,35 @@
+import { Payload } from "./Payload";
+import { Astronaut } from "./Astronaut";
+import { Cargo } from "./Cargo";
+
+export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    sumMass(items: Payload[]): number {
+        return items.reduce((accumulator, currentValue) => accumulator + currentValue.massKg, 0);
+    }
+    currentMassKg(): number {
+        return this.sumMass(this.cargoItems) + this.sumMass(this.astronauts);
+    }
+    canAdd(item: Payload): boolean {
+        return this.currentMassKg() + item.massKg <= this.totalCapacityKg;
+    }
+    addCargo(cargo: Cargo): boolean {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        } else return false;
+    }
+    addAstronaut(astronaut: Astronaut): boolean {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        } else return false;
+    }
+}
